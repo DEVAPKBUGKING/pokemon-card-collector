e6a4d389d4abb5a34f274834784b4065dfee377c
King <ryeebootxit@gmail.com>
2026-07-24 18:01:51 +0700

    Delete src/pages/Shop.jsx

--- src/pages/Shop.jsx
+++
@@ -1,8 +0,0 @@
-export default function Shop() {
-  return (
-    <div style={{ padding: 20, textAlign: 'center', marginTop: 40 }}>
-      <h2>🛒 Toko</h2>
-      <p>Segera hadir...</p>
-    </div>
-  );
-}
