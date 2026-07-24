3b4ada914fe08bf18e5970f3384cb3e5f2b1e184
King <ryeebootxit@gmail.com>
2026-07-24 15:50:53 +0700

    Add Backpack component with placeholder content

---
+++ src/pages/Backpack.jsx
@@ -0,0 +1,8 @@
+export default function Backpack() {
+  return (
+    <div style={{ padding: 20, textAlign: 'center', marginTop: 40 }}>
+      <h2>🎒 Backpack</h2>
+      <p>Segera hadir...</p>
+    </div>
+  );
+}
