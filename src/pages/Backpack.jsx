cf357b2615ee6c7a1ea2358c3143b8535c940ffb
King <ryeebootxit@gmail.com>
2026-07-24 18:00:36 +0700

    Delete src/pages/Backpack.jsx

--- src/pages/Backpack.jsx
+++
@@ -1,8 +0,0 @@
-export default function Backpack() {
-  return (
-    <div style={{ padding: 20, textAlign: 'center', marginTop: 40 }}>
-      <h2>🎒 Backpack</h2>
-      <p>Segera hadir...</p>
-    </div>
-  );
-}
