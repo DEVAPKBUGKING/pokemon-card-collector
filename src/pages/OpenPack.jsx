a4dbcf42e40bcc2b3dd2c7b697b4eaab5be43224
King <ryeebootxit@gmail.com>
2026-07-24 18:01:16 +0700

    Delete src/pages/OpenPack.jsx

--- src/pages/OpenPack.jsx
+++
@@ -1,8 +0,0 @@
-export default function OpenPack() {
-  return (
-    <div style={{ padding: 20, textAlign: 'center', marginTop: 40 }}>
-      <h2>🎴 Buka Pack</h2>
-      <p>Segera hadir...</p>
-    </div>
-  );
-}
